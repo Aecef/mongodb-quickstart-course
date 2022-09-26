4f6ecba4258c490356ac3b8c07ce2077dab3b7d6
Alec Cleofe <aleccleofe@gmail.com>
2022-09-26 14:41:57 -0500

    Initial Classes Setup

---
+++ src/starter_code_snake_bnb/src/data/bookings.py
@@ -0,0 +1,10 @@
+class Booking:
+    guest_owner_id = None
+    guest_snake_id = None
+
+    booked_date = None
+    checked_in_date = None
+    checked_out_date = None
+
+    review = None
+    rating = None
--- src/starter_code_snake_bnb/src/data/snakes.py
+++ src/starter_code_snake_bnb/src/data/snakes.py
@@ -1,8 +1,12 @@
-class Snakes:
+class Snake:
     registered_date = None
     species = None
 
     length = None
     name = None
     is_venomous = None
-    
+
+    meta = {
+        'db_alias': 'core',
+        'collection': 'snakes'
+    }
